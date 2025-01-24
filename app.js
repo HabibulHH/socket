a602ee8c53f6d7b953c5b1819e3b1feed76da9c5
habibul <habibul.hasan.hira@gmail.com>
2025-01-24 18:06:19 +0600

    added skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.emit('message', 'Welcome to the chat!');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
